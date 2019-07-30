ce544eadc0401b605abc4bd694ac2692a9c4f94c
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-07-30 18:22:00 -0500

    initialized files and added firebase to project

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDFoFJzUjr1pVRRGdpW6m2i_6WgZ3wmV3c",
+    authDomain: "fir-rps-27235.firebaseapp.com",
+    databaseURL: "https://fir-rps-27235.firebaseio.com",
+    projectId: "fir-rps-27235",
+    storageBucket: "",
+    messagingSenderId: "401582342921",
+    appId: "1:401582342921:web:57e349db9000daa7"
+  };
+
+  firebase.initializeApp(firebaseConfig);
